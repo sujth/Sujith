0b50c5b47164145f92db8fd86bd0edaddd8ae560
sujth <sujithk97@gmail.com>
2021-08-03 14:22:49 +0530

    folder structure

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,28 @@
+package org.add;
+
+public class GreensTech {
+	public void greensOmr() {
+		System.out.println("Greens omr");
+	}
+	public void greensAdayar() {
+		System.out.println("Greens Adayar");
+	}
+	public void greensTambaram() {
+		System.out.println("Greens Tambaram");
+	}
+	public void greensVelachery() {
+		System.out.println("Greens Velachery");
+	}
+	public void greensAnnaNagar() {
+		System.out.println("Greens AnnaNagar");
+	}
+	public static void main(String[] args) {
+		GreensTech g=new GreensTech();
+		g.greensOmr();
+		g.greensAdayar();
+		g.greensTambaram();
+		g.greensVelachery();
+		g.greensAnnaNagar();
+	}
+
+}
